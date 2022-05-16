df0568bc7e921beca1fd2abd4a5c2d84c3384d56
pds0309 <ehd0309@gmail.com>
2022-05-16 22:12:07 +0900

    feat: 공통 버튼 컴포넌트 추가

---
+++ movie_search/src/components/GlobalButton.js
@@ -0,0 +1,26 @@
+import { Borders, Colors } from '../styles';
+
+import styled from '@emotion/styled';
+
+// child: 버튼 텍스트
+// size: small, normal, medium, large
+// colorType: true(브랜드 컬러) | false(서브 컬러)
+const GlobalButton = ({ children, size = 'normal', colorType = false, ...props }) => {
+  const setButtonClassName = 'button is-' + size;
+  return (
+    <StyledButton className={setButtonClassName} colorType={colorType} {...props}>
+      {children}
+    </StyledButton>
+  );
+};
+
+const StyledButton = styled.button`
+  background-color: ${({ colorType }) => (!colorType ? Colors.mainColor : Colors.subColor)};
+  border-radius: ${Borders.buttonBorder};
+  :hover {
+    background-color: ${({ colorType }) =>
+      !colorType ? Colors.mainHoverColor : Colors.subHoverColor};
+  }
+`;
+
+export default GlobalButton;
