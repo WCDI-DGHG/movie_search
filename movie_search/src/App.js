dbff993bd46d232cf4e0238c63656d0dc7a05f35
pds0309 <ehd0309@gmail.com>
2022-05-16 20:40:54 +0900

    feat: 페이지 라우팅

--- movie_search/src/App.js
+++ movie_search/src/App.js
@@ -1,5 +1,15 @@
+import { HomePage, SearchPage } from './pages';
+import { Route, Routes } from 'react-router-dom';
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/search" element={<SearchPage />}></Route>
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
